7020b40f898f3d4b24f42c2e08b29fcffc8c8545
ilya448824 <sivkovila30@gmail.com>
2023-07-22 12:17:13 +0300

    Занятие 5

--- online_shop/app_online_shop/views.py
+++ online_shop/app_online_shop/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# функция отобр индекс.html
+
 def index(request):
-    return HttpResponse('Успешно!')
+    return render(request, 'index.html')
+
+# функция отображающая top-sellers.html
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
