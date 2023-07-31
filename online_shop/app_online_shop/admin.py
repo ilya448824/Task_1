d2afc807cdea2184ac75ec5907b70f4e062fd227
ilya448824 <sivkovila30@gmail.com>
2023-07-31 19:57:28 +0300

    Занятие 7

--- online_shop/app_online_shop/admin.py
+++ online_shop/app_online_shop/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from  .models import OnlineShop
 # Register your models here.
+
+# класс для отображения панели администрирования
+class OnlineShopAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'creation_time','auction']
+    list_filter = ['auction', 'creation_time']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+
+    fieldsets = (
+        ('Общее', {
+            'fields': ('title', 'description')
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+        })
+    )
+
+    @admin.action(description='Убрать возможнось торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+    @admin.action(description='Добавить возможнось торга')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+# отображение нашу модель в панель администрирования
+admin.site.register(OnlineShop, OnlineShopAdmin)
--- online_shop/app_online_shop/models.py
+++ online_shop/app_online_shop/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+# Создаём класс с описанием структуры будующей таблицы (наследуемся о класса Model)
+class OnlineShop(models.Model):
+        # Заголовок
+        # CharField класс, обозначающий символьное поле (набор символов), для небольших текстов
+        title = models.CharField('Заголовок', max_length=128)
+        # описание объявления
+        # TextField класс, обозначающий строковое поле больших размеров
+        description = models.TextField('Описание')
+        # цена
+        # DecimalField дробное число с фиксированной точностью (похоже на float в Python)
+        # max_digits максимальное кол-во цифр в числе
+        # decimal_places максимальное кол-во знаков после запятой
+        price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+        # возможность торговаться
+        # BooleanField логический тип данных (истина или ложь)
+        auction = models.BooleanField('Торг', help_text='Отметьте, уместен ли торг')
+        # дата размещения объявления
+        # auto_now_add=True сразу получаем дату в момент создания объявления
+        creation_time = models.DateTimeField(auto_now_add=True)
+        # дата обновления объявления
+        # auto_now=True получаем дату в момент обновления объявления
+        update_time = models.DateTimeField(auto_now=True)
+        def __str__(self):
+              return f"OnlineShop(id={self.id}, title={self.title}, price={self.price})"
+        class Meta:
+            db_table = 'advertisements'
